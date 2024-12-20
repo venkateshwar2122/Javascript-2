f2a68c2119ba509690c5e98671cedca52e133ff3
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-12-20 12:27:45 +0530

    Create 1.ext1.js

---
+++ 016. MODULAR  FUNCTION/001.require,exports/1.ext1.js
@@ -0,0 +1,9 @@
+// note that this file extension is .js and not .mjs
+
+let add = require("./2.demo1.js")
+add(2,3)
+
+/* let {add,fact,gcd} = require("./2.demo1.js")
+add(10,10)
+fact(3)
+gcd(9,81)  */
