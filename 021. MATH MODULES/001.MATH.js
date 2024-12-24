fa7e27280c4a413d0e94d43a5ff310d041eb9e41
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-12-24 13:37:01 +0530

    Update 001.MATH.js

--- 021. MATH MODULES/001.MATH.js
+++ 021. MATH MODULES/001.MATH.js
@@ -1,8 +1,8 @@
 console.log(Math.abs(-9))          // -ve to +ve number
 console.log(Math.round(3.6))
 console.log(Math.round(3.4))
-console.log(Math.ceil(3.3))         // 1 less
-console.log(Math.floor(3.2))       // 1 more
+console.log(Math.ceil(3.3))         // 1 more
+console.log(Math.floor(3.2))       // 1 less
 console.log(Math.sqrt(6))         // squarre root
 console.log(Math.random())        // generate random number
 console.log(Math.random()*9999+10000)   //generate exact 5 digit random number
